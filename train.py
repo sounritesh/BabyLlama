296a3ba947966a20811f92a733b26a8f1e27c480
Ritesh Soun <sounritesh@gmail.com>
2023-09-27 17:56:31 +0530

    fix: tie word embeddings fix

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from transformers import (
-    GPT2Config, GPT2LMHeadModel, 
-    LlamaConfig, LlamaForCausalLM, 
+    GPT2Config, GPT2LMHeadModel,
+    LlamaConfig, LlamaForCausalLM,
     GPTJConfig, GPTJForCausalLM
 )
 from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
@@ -14,9 +14,9 @@ import argparse
 from babylm_dataset import BabylmDataset
 
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument("--config", type=str, default="./config/llama-16M.yaml", help="Configuration file path")
+parser.add_argument("--config", type=str,
+                    default="./config/llama-16M.yaml", help="Configuration file path")
 parser.add_argument("--lr", type=float, default=None, help="Learning rate")
 parser.add_argument("--model_name", type=str, default=None, help="Model name")
 args = parser.parse_args()
@@ -35,18 +35,21 @@ if args.model_name:
 SEQ_LENGTH = config['data']['seq_length']
 
 tokenizer_path = config['data']['tokenizer_path']
-tokenizer = GPT2TokenizerFast(tokenizer_file= str(tokenizer_path))
+tokenizer = GPT2TokenizerFast(tokenizer_file=str(tokenizer_path))
 tokenizer.bos_token = "<s>"
 tokenizer.eos_token = "</s>"
 tokenizer.pad_token = "<pad>"
 
 # in the original code I had random_chunk = False
 # random_chunk=True is expected to improve the model performance a bit
-train_dataset = BabylmDataset(config['data']['train_path'], SEQ_LENGTH, tokenizer=tokenizer, random_chunk=True)
-full_eval_dataset = BabylmDataset(config['data']['eval_path'], SEQ_LENGTH, tokenizer=tokenizer, offset=0)
+train_dataset = BabylmDataset(
+    config['data']['train_path'], SEQ_LENGTH, tokenizer=tokenizer, random_chunk=True)
+full_eval_dataset = BabylmDataset(
+    config['data']['eval_path'], SEQ_LENGTH, tokenizer=tokenizer, offset=0)
 
-seed(2023) # we fix the same subset for all models
-eval_indices = sample(range(len(full_eval_dataset)), config['data']['eval_samples'])
+seed(2023)  # we fix the same subset for all models
+eval_indices = sample(range(len(full_eval_dataset)),
+                      config['data']['eval_samples'])
 eval_dataset = Subset(full_eval_dataset, eval_indices)
 
 # We tokenize the whole dataset and then set the max length
@@ -65,7 +68,7 @@ if config['model']['type'] == "Llama":
         intermediate_size=config['model']['intermediate_size'],
         num_hidden_layers=config['model']['n_layer'],
         num_attention_heads=config['model']['n_head'],
-        tie_word_embeddings=config['model']['tie_word_embeddings'],
+        tie_word_embeddings=False,
         pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
     )
     model = LlamaForCausalLM(model_config)
@@ -76,9 +79,9 @@ elif config['model']['type'] == "GPT2":
         n_embd=config['model']['hidden_size'],
         n_layer=config['model']['n_layer'],
         n_head=config['model']['n_head'],
-        resid_pdrop = config['model']['resid_pdrop'],
-        embd_pdrop = config['model']['embd_pdrop'],
-        attn_pdrop = config['model']['attn_pdrop'],
+        resid_pdrop=config['model']['resid_pdrop'],
+        embd_pdrop=config['model']['embd_pdrop'],
+        attn_pdrop=config['model']['attn_pdrop'],
         pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
     )
     model = GPT2LMHeadModel(model_config)
@@ -89,9 +92,9 @@ elif config['model']['type'] == "GPTJ":
         n_embd=config['model']['hidden_size'],
         n_layer=config['model']['n_layer'],
         n_head=config['model']['n_head'],
-        resid_pdrop = config['model']['resid_pdrop'],
-        embd_pdrop = config['model']['embd_pdrop'],
-        attn_pdrop = config['model']['attn_pdrop'],
+        resid_pdrop=config['model']['resid_pdrop'],
+        embd_pdrop=config['model']['embd_pdrop'],
+        attn_pdrop=config['model']['attn_pdrop'],
         tie_word_embeddings=config['model']['tie_word_embeddings'],
         pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
     )
@@ -107,13 +110,13 @@ per_device_bsz = config['training']['batch_size'] // accumulation_steps
 training_args = TrainingArguments(
     output_dir=output_dir,
     overwrite_output_dir=True,
-    save_strategy = "epoch",
-    evaluation_strategy = "epoch",
+    save_strategy="epoch",
+    evaluation_strategy="epoch",
     num_train_epochs=4,
     gradient_accumulation_steps=accumulation_steps,
     per_device_train_batch_size=per_device_bsz,
     save_total_limit=1,  # Set to zero to avoid saving
-    warmup_steps=config['training']['warmup_steps'], 
+    warmup_steps=config['training']['warmup_steps'],
     lr_scheduler_type="cosine",
     learning_rate=float(config['training']['lr']),
     logging_steps=10,
@@ -136,7 +139,8 @@ if __name__ == "__main__":
     if config['logging']['wandb']:
         import wandb
         wandb.login()
-        wandb.init(project= config['logging']['project'], name=config['model']['name'], config=config)
+        wandb.init(project=config['logging']['project'],
+                   name=config['model']['name'], config=config)
 
     trainer.train()
     trainer.save_model(output_dir)
